494a951765c18d7d61da9edb5e2c389356895b7f
Costelas Denis <81863134+denosg@users.noreply.github.com>
2023-12-09 20:38:23 +0200

    Update mongoose.js

--- dist/src/db/mongoose.js
+++ dist/src/db/mongoose.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config({ path: 'config/dev.env' });
+
 const databaseUrl = process.env.DATABASE_URL;
 mongoose.connect(databaseUrl);
 //# sourceMappingURL=mongoose.js.map
